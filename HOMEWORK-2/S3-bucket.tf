0d73b206382732593f8acde31d658493be343191
aigerim-199 <aigerim.zhantoro@gmail.com>
2025-01-17 21:16:42 +0000

    added 2nd homework

--- HOMEWORK-2/S3-bucket.tf
+++ HOMEWORK-2/S3-bucket.tf
@@ -1,4 +1,14 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "bucket" {
   bucket = "kaizen-aigerimz"
 }
 
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "kaizen-bucket1"
+}
+# terraform import aws_s3_bucket.bucket1 kaizen-bucket1
+
+
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "kaizen-bucket2"
+}
+# terraform import aws_s3_bucket.bucket2 kaizen-bucket2
