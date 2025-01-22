f29cb9d3b15f82336c48fab1db6dcc828df29fc7
aigerim-199 <aigerim.zhantoro@gmail.com>
2025-01-22 20:54:33 +0000

    added 3rd homework

--- HOMEWORK-3/main.tf
+++ HOMEWORK-3/main.tf
@@ -5,6 +5,7 @@ provider aws {
 resource "aws_key_pair" "deployer" {
   key_name   = "bastion-key"
   public_key = file("~/.ssh/id_rsa.pub")
+
 }
 
 data "aws_ami" "amazon_linux2" {
@@ -23,46 +24,49 @@ data "aws_ami" "amazon_linux2" {
   owners = ["137112412989"] # Canonical
 }
 
+
 #1-instance
 resource "aws_instance" "web1" {
   ami           = data.aws_ami.amazon_linux2.id
   instance_type = "t2.micro"
-  availability_zone = "us-east-2c" # Do we need to specify here az, if I am using different subnets in different zones?
-  subnet_id = "subnet-01b10247a846df36b"
+  #availability_zone = "us-east-2a"
+  subnet_id = "subnet-0923c8f1a28b28e90"
   key_name = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   user_data = file("apache.sh")
+  
+ 
 
-
-  tags = {
+   tags = {
     Name = "web-1"
-  }
-}
 
-output ec2-1 {
+  }
+  }
+
+  output ec2-1 {
     value = aws_instance.web1.public_ip
     sensitive = true
 
 }
 
+
 #2-instance
 resource "aws_instance" "web2" {
   ami           = data.aws_ami.amazon_linux2.id
   instance_type = "t2.micro"
-  availability_zone = "us-east-2b" 
-  subnet_id = "subnet-01c2612599004c5fb"
-  key_name = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  user_data = file("apache.sh")
+  #availability_zone = "us-east-2b"
+  subnet_id = "subnet-0d496f31e9a9818a2"
+   key_name = aws_key_pair.deployer.key_name
+   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+   user_data = file("apache.sh")
 
-
-  tags = {
+   tags = {
     Name = "web-2"
   }
 }
 
 output ec2-2 {
-    value = aws_instance.web2.public_ip
+    value = aws_instance.web1.public_ip
     sensitive = true
 
 }
@@ -71,23 +75,20 @@ output ec2-2 {
 resource "aws_instance" "web3" {
   ami           = data.aws_ami.amazon_linux2.id
   instance_type = "t2.micro"
-  availability_zone = "us-east-2a" 
-  subnet_id = "subnet-0e17387c5865a8cfc"
-  key_name = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  user_data = file("apache.sh")
+  #availability_zone = "us-east-2c"
+  subnet_id = "subnet-0bcffea6caebbf5ab"
+   key_name = aws_key_pair.deployer.key_name
+   vpc_security_group_ids = [aws_security_group.allow_tls.id]
+   user_data = file("apache.sh")
 
 
-  tags = {
+   tags = {
     Name = "web-3"
   }
-
 }
 
-  output ec2-3 {
-    value = aws_instance.web3.public_ip
+output ec2-3 {
+    value = aws_instance.web1.public_ip
     sensitive = true
 
 }
-
-  
